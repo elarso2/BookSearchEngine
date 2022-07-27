2cfbefe1f1a16a93a1483adf9e476e1f0437be25
Elizabeth Larson <elarso2@uw.edu>
2022-07-27 16:52:36 -0700

    complete typedefs, starting resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,8 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {},
+};
+
+module.exports = typeDefs;
