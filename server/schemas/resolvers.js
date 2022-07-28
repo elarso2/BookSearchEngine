c1ad67c5249931c341bdc3a25097dfaa0341d4ab
Elizabeth Larson <elarso2@uw.edu>
2022-07-27 17:00:39 -0700

    resolver me query, adduser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,24 @@
 const { AuthenticationError } = require("apollo-server-express");
+const { User, Book } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
-  Query: {},
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
 };
 
-module.exports = typeDefs;
+module.exports = resolvers;
