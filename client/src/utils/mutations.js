1c7b8ee927301cc1659b2f690fa3b484effb5cf9
Elizabeth Larson <elarso2@uw.edu>
2022-07-27 17:38:06 -0700

    user mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,13 +2,45 @@ import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
     mutation loginUser($email: String!, $password: String!){
-        loginUser($email: String!, $password: String!)
+        loginUser($email: String!, $password: String!){
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    authors
+                    description
+                    title
+                    image
+                    link
+                }
+            }
+        }
     }
 `;
 
 export const ADD_USER = gql`
     mutation addUser($username: String! $email: String!, $password: String!) {
-        addUser($username: String! $email: String!, $password: String!)
+        addUser($username: String! $email: String!, $password: String!) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    authors
+                    description
+                    title
+                    image
+                    link
+                }
+            }
+        }
     }
 `;
 
